3e8a2eaa8ab12b6385f0238699c7475e11889b4d
Daniele <danielecamodeca@192.168.1.60>
2023-04-23 20:54:31 +0200

    2prog

--- Progetti/Meteo/Meteo.js
+++ Progetti/Meteo/Meteo.js
@@ -1,72 +1,75 @@
-const api = {
-    key:"cf7543021c5096cf8d75b92eeb63e205",
-    base: "https://api.openweathermap.org/data/3.0/"
-}
-const search = document.querySelector(".search");
-const btn = document.querySelector(".btn");
-btn.addEventListener("click", getInput);
+const apiKey = "cf7543021c5096cf8d75b92eeb63e205";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
-function getInput (event) {
-    event.preventDefault();
-    if (event.type == "click"){
-        getData(search.value);
-        console.log(search.value);
 
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    if(response.status == 404){
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else{
+        var data = await response.json();
+        document.querySelector(".city").innerHTML= data.name;
+        document.querySelector(".temp").innerHTML=Math.round(data.main.temp) +"°c";
+        document.querySelector(".humidity").innerHTML= data.main.humidity+"%";
+        document.querySelector(".wind").innerHTML= data.wind.speed+"km/h";
+       
+        if(data.weather[0].main=="Clouds"){
+            weatherIcon.src = "./immagini/clouds.png";
+            
+       }
+       else if(data.weather[0].main =="Clear"){
+        weatherIcon.src = "./immagini/clear.png";
+       }
+       else if(data.weather[0].main =="Rainar"){
+        weatherIcon.src = "./immagini/rain.png";
+       }
+       else if(data.weather[0].main =="Drizzle"){
+        weatherIcon.src = "./immagini/drizzle.png";
+       }
+       else if(data.weather[0].main =="Mist"){
+        weatherIcon.src = "./immagini/mist.png";
+       }
+       document.querySelector(".weather").style.display ="block";
+       document.querySelector(".error").style.display ="none";
 
     }
-}
-function getData(){
-    fetch(`${api.base}weathwer?q=${search.value}&
-    units=metric&appid=${api.key}`)
-       .then(response =>{
-            return response.json();
+        var data = await response.json();
+        
 
-    }).then(displayData);
-
-}
-function displayData (response){
-    //console.log(response);
-    if (response.cod === "404"){
-        const error = document.querySelector(".error");
-        error.textContent="Inserisci una città valida";
-        search.value="";
-    }else {
-        const city = document.querySelector(".city");
-        city.innerText = `${response.name},${response.sys.country}`;
-
-        const today= new Date();
-        const date= document.querySelector(".date");
-        date.innerText=dateFunction(today);
-
-        const temp = document.querySelector(".temp");
-        temp.innerHTML= `Temp:${Math.round(response.main,temp)}
-        <span>°C</span> `; 
-        const weather = document.querySelector(".weather");
-        weather.innerText=`weather: ${response.weather[0],
-        main}`;
-
-        const tempRange = document.querySelector(".temp-range");
-        tempRange.innerText=`Temp Range:${Math.round(response.main.temp_min)}°C/
-        ${Math.round(response.main.temp_max)}°C`;
-
-        constweatherIcon = document.querySelector(".weather-icon");
-        const iconURL ="http://openweathermap.org/img/w/";
-        weatherIcon.src =iconURL + response.weather[0].
-        icon+".png";
-        search.value ="";
-    }
-}
- function dateFunction (d){
-    let month = ["Gen","Feb","Mar","Apr","Mag",
-    "Giu","Lug","Ago","Set","Ott","Nov","Dic"];
-
-    let days =["Lun","Mar","Mer",
-    "Gio","Ven","Sab","Dom"];
-
-    let day =days[d.getDay()];
-    let date =d.getDate();
-    let month =d.getMonth();
-    let year = d.getFullYear();
     
-    return `${day},${month},${year}`; 
- }
+   
+
+
+    document.querySelector(".city").innerHTML= data.name;
+    document.querySelector(".temp").innerHTML=Math.round(data.main.temp) +"°c";
+    document.querySelector(".humidity").innerHTML= data.main.humidity+"%";
+    document.querySelector(".wind").innerHTML= data.wind.speed+"km/h";
+   
+    if(data.weather[0].main=="Clouds"){
+        weatherIcon.src = "./immagini/clouds.png";
+        
+   }
+   else if(data.weather[0].main =="Clear"){
+    weatherIcon.src = "./immagini/clear.png";
+   }
+   else if(data.weather[0].main =="Rainar"){
+    weatherIcon.src = "./immagini/rain.png";
+   }
+   else if(data.weather[0].main =="Drizzle"){
+    weatherIcon.src = "./immagini/drizzle.png";
+   }
+   else if(data.weather[0].main =="Mist"){
+    weatherIcon.src = "./immagini/mist.png";
+   }
+   document.querySelector(".weather").style.display ="block";
+   document.querySelector(".error").style.display = "none";
+    
+    
+}
+searchBtn.addEventListener("click",() =>{
+    checkWeather(searchBox.value);
+})
