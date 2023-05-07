3fcc0e94f3f2899d69e3ce3591371d70354e1e83
Daniele <danielecamodeca@192.168.1.60>
2023-05-07 12:03:12 +0200

    new update

--- Progetti/JumpGame/JavaScript/run.js
+++ Progetti/JumpGame/JavaScript/run.js
@@ -1,6 +1,14 @@
 const hero = document.querySelector(".hero");
 const heroG = document.querySelector(".heroG");
 const iron = document.querySelector(".iron");
+var score = 0;
+
+function updateScore() {
+    score++;
+    document.getElementById("score").innerHTML = "Score: " + score;
+}
+
+
 
 const jump = () => {
     if (hero.classList != "animate") {
@@ -9,30 +17,36 @@ const jump = () => {
 
     }
     setTimeout(() => {
-        hero.classList.remove("animate"); 
+        hero.classList.remove("animate");
     }, 300);
-};
+    updateScore();
 
-document.addEventListener("keydown" , (e) => {
+};
+setInterval(() => {
+    updateScore();
+}, 1000);
+
+document.addEventListener("keydown", (e) => {
     if (e.code == "Space") {
         jump();
     }
 
 });
-window.addEventListener("click" , () => {
-     {jump(); }
+window.addEventListener("click", () => {
+    { jump(); }
 
 });
-let isAlive = setInterval (() => {
+let isAlive = setInterval(() => {
     let heroTop = parseInt(window.getComputedStyle(hero).
-    getPropertyValue("top"));
+        getPropertyValue("top"));
     // console.log(heroTop);
     let ironLeft = parseInt(window.getComputedStyle(iron).
-    getPropertyValue("left"));
-    if(ironLeft < 40 && ironLeft > 20 && heroTop >=130){
+        getPropertyValue("left"));
+
+    if (ironLeft < 30 && ironLeft > 20 && heroTop >= 130) {
         iron.style.animation = "none"
         alert("Game Over!!Press spacebar to start");
-
+        score = 0;
+        document.getElementById("score").innerHTML = "Score: " + score;
     }
-
 }, 10);
