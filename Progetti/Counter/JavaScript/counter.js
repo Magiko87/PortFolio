436771c5ca608c32fa04d52f6ebdb27cf8eb51ed
Daniele <danielecamodeca@192.168.1.60>
2023-05-13 10:28:28 +0200

    m

--- Progetti/Counter/JavaScript/counter.js
+++ Progetti/Counter/JavaScript/counter.js
@@ -1,57 +1,104 @@
-const count=document.querySelector(".count");
-const add=document.querySelector(".add");
-const resetCount=document.querySelector(".reset");
-const sub=document.querySelector(".subtract");
+const buttons = [];
 
-function setColor(){
-    if(count.innerHTML >0){
-        count.style.color = " green";}
-        else if (count.innerHTML<0){
-            count.style.color = "red";
+let counter = 0;
 
-        }else{
-            count.style.color ="white";
-        }
-           
-    
-    }
+const count = document.querySelector("#count");
 
-
-function keysPressed(){
-    if(event.key == "ArrowLeft"){
-        count.innerHTML--;
-        setColor()
-    }
-
-    if(event.key == "ArrowRight"){
-        count.innerHTML++;
-        setColor()
-    }
-    if(event.key == "Enter"){
-        count.innerHTML= 0;
-        setColor()
-    }
-    
+function updateCounter() {
+  count.innerText = counter;
 }
 
+updateCounter();
+
+let buttonsContainer = document.querySelector('.buttons');
 
 
-window.addEventListener("keydown",
-keysPressed)
+// Inizio crazione dei bottoni dinamicamente
+const subtractButton = document.createElement("button");
+subtractButton.innerText = "Subtract";
+subtractButton.classList.add("subtract", "btn");
+
+const addButton = document.createElement("button");
+addButton.innerText = "Add";
+addButton.classList.add("add", "btn");
+
+const resetButton = document.createElement("button");
+resetButton.innerText = "Reset";
+resetButton.classList.add("reset", "btn");
 
 
+// Fine creazione bottoni js
 
 
-add.addEventListener("click", ()  => {
-    count.innerHTML++;
-    setColor()
-})
-sub.addEventListener("click", ()  => {
-    count.innerHTML--;
-    setColor()
-})
-resetCount.addEventListener("click", ()  => {
-    count.innerHTML= 0;
-    setColor()
-})
- 
+subtractButton.addEventListener("click", () =>{
+    counter--;
+    setColor();
+    updateCounter()
+    sound();
+});
+
+resetButton.addEventListener("click", () =>{
+    counter = 0;
+    setColor();
+    updateCounter()
+    sound();
+});
+
+addButton.addEventListener("click", () =>{
+    counter++;
+    setColor();
+    updateCounter();
+          sound();
+
+});
+
+document.addEventListener("keydown", function(event) {
+    if(event.key == "ArrowLeft") {
+      counter--;
+      setColor();
+      updateCounter();
+      sound();
+    }
+    if(event.key == "ArrowRight") {
+      counter++;
+      setColor();
+      updateCounter();
+      sound();
+
+    }
+    if(event.key == "Enter") {
+      counter = 0;
+      setColor();
+      updateCounter();
+      sound();
+
+    }
+});
+
+buttons.push(subtractButton);
+buttons.push(resetButton);
+buttons.push(addButton);
+
+document.addEventListener('DOMContentLoaded', () => {
+  buttons.forEach((button) => {
+    buttonsContainer.appendChild(button)
+  });
+});
+
+function setColor(){
+    if(counter > 0){
+        count.style.color = "green";
+    } else if (counter < 0){
+        count.style.color = "red";
+    } else{
+        count.style.color = "white";
+    }
+}
+function sound() {
+  const audio = new Audio("./Suoni/mixkit-handgun-click-1660.mp3");
+  audio.currentTime = 0.0;
+  audio.play();
+  setTimeout(function() {
+      audio.pause();
+  }, 1000);
+}
